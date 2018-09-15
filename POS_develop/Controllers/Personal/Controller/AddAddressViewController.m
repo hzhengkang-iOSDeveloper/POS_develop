f873da8cb5e102b865da7506d066ccdd08f05485
syn <sunyanan_ios@163.com>
2018-09-15 14:20:18 +0800

    firstCommit

--- POS_develop/Controllers/Personal/Controller/AddAddressViewController.m
+++ POS_develop/Controllers/Personal/Controller/AddAddressViewController.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) UITextField *telephoneTF;
 @property (nonatomic, strong) UIButton *cityBtn;
 @property (nonatomic, strong) UITextField *detailAddress;
+@property (nonatomic, strong) UIButton *defaultAddressBtn;
 @end
 
 @implementation AddAddressViewController
@@ -70,7 +71,6 @@
     self.telephoneTF.font = F13;
     self.telephoneTF.layer.cornerRadius = FITiPhone6(5);
     self.telephoneTF.layer.masksToBounds = YES;
-//    self.telephoneTF.backgroundColor = WhiteColor;
     [whiteBgView addSubview:_telephoneTF];
     [_telephoneTF mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view).offset(FITiPhone6(15));
@@ -86,6 +86,88 @@
         make.size.sizeOffset(CGSizeMake(ScreenWidth - FITiPhone6(15), FITiPhone6(0.5)));
     }];
     self.cityBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.cityBtn setTitle:@"请选择" forState:UIControlStateNormal];
+    self.cityBtn.titleLabel.font = F13;
+    [self.cityBtn setTitleColor:C989898 forState:UIControlStateNormal];
+    [whiteBgView addSubview:self.cityBtn];
+    [self.cityBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(FITiPhone6(15));
+        make.top.equalTo(line2.mas_bottom);
+        make.size.mas_offset(CGSizeMake(ScreenWidth - FITiPhone6(15), FITiPhone6(46)));
+    }];
+    UIView *line3 = [[UIView alloc] init];
+    line3.backgroundColor = CE6E2E2;
+    [whiteBgView addSubview:line3];
+    [line3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(FITiPhone6(15));
+        make.top.equalTo(self.cityBtn.mas_bottom);
+        make.size.sizeOffset(CGSizeMake(ScreenWidth - FITiPhone6(15), FITiPhone6(0.5)));
+    }];
+    self.detailAddress = [[UITextField alloc] init];
+    self.detailAddress.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, FITiPhone6(5), 0)];
+    self.detailAddress.leftViewMode = UITextFieldViewModeAlways;
+    self.detailAddress.placeholder = @"请输入详细地址";
+    [self.detailAddress setValue:C989898
+                    forKeyPath:@"_placeholderLabel.textColor"];
+    [self.detailAddress setValue:F13 forKeyPath:@"_placeholderLabel.font"];
+    self.detailAddress.keyboardType = UIKeyboardTypeNumberPad;
+    self.detailAddress.borderStyle = UITextBorderStyleNone;
+    self.detailAddress.font = F13;
+    self.detailAddress.layer.cornerRadius = FITiPhone6(5);
+    self.detailAddress.layer.masksToBounds = YES;
+    [whiteBgView addSubview:_detailAddress];
+    [_detailAddress mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(FITiPhone6(15));
+        make.top.equalTo(line3.mas_bottom);
+        make.size.mas_offset(CGSizeMake(ScreenWidth - FITiPhone6(15), FITiPhone6(46)));
+    }];
+    self.defaultAddressBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.defaultAddressBtn setTitle:@"设置为默认地址" forState:UIControlStateNormal];
+    [self.defaultAddressBtn setTitleColor:C090909 forState:UIControlStateNormal];
+    [self.defaultAddressBtn layoutButtonWithEdgeInsetsStyle:MKButtonEdgeInsetsStyleLeft imageTitleSpace:FITiPhone6(23)];
+    [self.defaultAddressBtn setImage:[UIImage imageNamed:@"默认"] forState:normal];
+    [self.defaultAddressBtn setImage:[UIImage imageNamed:@"默认地址"] forState:UIControlStateSelected];
+    [self.defaultAddressBtn addTarget:self action:@selector(defaultAddressClick:) forControlEvents:UIControlEventTouchUpInside];
+    self.defaultAddressBtn.titleLabel.font = F12;
+    [self.view addSubview:self.defaultAddressBtn];
+    [self.defaultAddressBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).offset(FITiPhone6(25));
+        make.bottom.equalTo(self).offset(FITiPhone6(-15));
+        make.size.sizeOffset(CGSizeMake(FITiPhone6(80), FITiPhone6(15)));
+    }];
+    UIButton *saveBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    saveBtn.backgroundColor = C00A0E9;
+    [saveBtn setTitle:@"保存" forState:UIControlStateNormal];
+    [saveBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    saveBtn.layer.cornerRadius = FITiPhone6(3);
+    saveBtn.layer.masksToBounds = YES;
+    saveBtn.layer.cornerRadius = FITiPhone6(20);
+    saveBtn.layer.masksToBounds = YES;
+    [saveBtn addTarget:self action:@selector(saveClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:saveBtn];
+    [saveBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(FITiPhone6(15));
+        make.top.equalTo(self.defaultAddressBtn.mas_bottom).offset(FITiPhone6(61));
+        make.size.mas_offset(CGSizeMake(ScreenWidth - FITiPhone6(30), FITiPhone6(45)));
+    }];
 }
 
+#pragma mark ---- 设置为默认地址 ----
+- (void)defaultAddressClick:(UIButton *)sender {
+    
+}
+#pragma mark ---- 保存 ----
+- (void)saveClick {
+    
+}
 @end
+
+
+
+
+
+
+
+
+
+
