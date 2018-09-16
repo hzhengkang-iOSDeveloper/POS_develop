c4865b6d76246b1d1a4012cd1dd6c9523d547c99
hzhengkang-iOSDeveloper <hzhengkang@163.com>
2018-09-16 21:52:58 +0800

    我的订单

---
+++ POS_develop/Controllers/Personal/Controller/MyBill/PD_BillListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PD_BillListViewController.h
+//  POS_develop
+//
+//  Created by 孙亚男 on 2018/9/16.
+//  Copyright © 2018年 sunyn. All rights reserved.
+//
+
+#import "BaseSonViewController.h"
+
+@interface PD_BillListViewController : BaseSonViewController
+
+@end
---
+++ POS_develop/ToolsClass/ToolClass/SLConst.m
@@ -0,0 +1,18 @@
+//
+//  SLConst.m
+//  Shopping2.0
+//
+//  Created by chenpeng on 17/3/2.
+//  Copyright © 2017年 HongHui(Shanghai)Information Technology Service Co.,Ltd. All rights reserved.
+//
+
+#import "SLConst.h"
+
+
+
+const int tabIconWidth = 29; // Tabbar Icon 的宽度
+const int tabBadgeTop = 9; // Tabbar Badge Y
+
+const int tabH = 49; // tabbar 的高度
+const int navBarH = 44;  //导航栏无status的高度
+
